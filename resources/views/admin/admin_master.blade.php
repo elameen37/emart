3a2949fdb67ab4382e67cdc36cb4c470b2e8362c
elameen37 <elameen37@gmail.com>
2024-01-22 19:44:27 +0100

    Commented out dark mode CSS links in admin_master.blade.php

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -22,14 +22,14 @@
 
     <!-- Bootstrap Css -->
     <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('backend/assets/css/bootstrap-dark.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- <link href="{{ asset('backend/assets/css/bootstrap-dark.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" /> -->
 
     <!-- Icons Css -->
     <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 
     <!-- App Css-->
     <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('backend/assets/css/app-dark.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- <link href="{{ asset('backend/assets/css/app-dark.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" /> -->
 
 </head>
 
